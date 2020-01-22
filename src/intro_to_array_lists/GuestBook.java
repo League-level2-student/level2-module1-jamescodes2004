512ada7d08d760c75155b5e93bdeb971e99f9e98
League Student <league@iMac-37.attlocal.net>
2020-01-21 19:57:09 -0800

    1/21/20
    1/21/20

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,20 +1,53 @@
 package intro_to_array_lists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class GuestBook {
-	public static void main(String[] args) {
-	ArrayList<String> names = new ArrayList<String>();
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton();
+	JButton view = new JButton();
 	
-	names.add("Arman Dahi");
-	names.add("Graham");
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
-	// all the names added to the list. Format the list as follows:
-	// Guest #1: Bob Banders
-	// Guest #2: Sandy Summers
-	// Guest #3: Greg Ganders
-	// Guest #4: Donny Doners
-}
-}
+	ArrayList<String> names = new ArrayList<String>();
+	void run() {
+		add.setText("Add name");
+		view.setText("View names");
+		names.add("Arman Dahi");
+		names.add("Graham Winter");
+		names.add("James Ward");
+		names.add("Bob Banders");
+		add.addActionListener(this);
+		view.addActionListener(this);
+		panel.add(add);
+		panel.add(view);
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==add) {
+			String name = JOptionPane.showInputDialog(null, "What name would you like to add?");
+			names.add(name);
+		}
+		else if (e.getSource()==view){
+			String nam ="";
+			for (String na : names) {
+				nam = nam + na + " ";
+				
+			}
+			System.out.println(nam);
+			JOptionPane.showMessageDialog(null, nam);
+		}
+		}
+	}
